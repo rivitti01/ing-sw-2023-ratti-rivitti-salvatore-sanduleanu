5598a582bda7c95dd8cbbbbd4d78d22ee0fe690d
SAAL01 <alex.salv@outlook.it>
2023-03-19 21:39:54 +0100

    Modifica dell'artifactId

---
+++ ingSw/src/main/java/it/polimi/ingsw/model/CommonGoal7.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.model;
+
+public class CommonGoal7 implements CardStrategy {
+
+    public boolean algorythm(Shelf myShelf) {
+        //le diagonali possibili sono 4, le controllo una per volta
+        for(int i=0; i<2; i++) {
+            for(int j=0; myShelf.getTile(i+j,i+j).getColor() == myShelf.getTile(i+j+1,i+j+1).getColor(); j++)
+                if(j==3) return true;
+        }
+
+        for(int i=4; i<6; i++) {
+            for(int j=0; myShelf.getTile(i+j,i+j).getColor() == myShelf.getTile(i+j-1,i+j+1).getColor(); j++)
+                if(j==3) return true;
+        }
+
+        return false;
+    }
+
+
+    public String toString() {
+        return "Cinque tessere dello stesso tipo che formano una diagonale.";
+    }
+
+}
