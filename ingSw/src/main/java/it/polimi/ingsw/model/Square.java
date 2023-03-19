8d41e2738ff236123d364e28176c6ce3ac000b21
Leonardo Ratti <leonardo.ratti@mail.polimi.it>
2023-03-19 17:50:24 +0100

    Aggiustato la classe Square

--- ingSw/src/main/java/it/polimi/ingsw/model/Square.java
+++ ingSw/src/main/java/it/polimi/ingsw/model/Square.java
@@ -3,23 +3,23 @@ package it.polimi.ingsw.model;
 public class Square implements CardStrategy{
 
     public boolean algorythm(Shelf myShelf) {
+        Tile[][] copiedShelf = myShelf.getShelf();
         int squareCounter = 0;
-        boolean squareFlag = false;
-        int squareColumn=0;
-        int r,c = 0;
+        int r = 0 ,c = 0;
         for (r=0; r<4; r++){
-            if (squareFlag){
-                c=squareColumn;
-                squareFlag=false;
-            }
-            for(c=0; c<3; c++){
-                if(myShelf.getTile(r, c).getColor().equals(myShelf.getTile(r+1, c+1).getColor()) && myShelf.getTile(r+1, c+1).getColor().equals(myShelf.getTile(r+1, c).getColor()) && myShelf.getTile(r+1, c).getColor().equals(myShelf.getTile(r, c+1).getColor())){
+            for(c= 0; c<3; c++){
+                if(copiedShelf[r][c].getColor()!=null
+                        && copiedShelf[r][c].getColor().equals(copiedShelf[r+1][c].getColor())
+                        && copiedShelf[r+1][c].getColor().equals(copiedShelf[r][c+1].getColor())
+                        && copiedShelf[r][c+1].getColor().equals(copiedShelf[r+1][c+1].getColor())) {
+                    copiedShelf[r][c] = null;
+                    copiedShelf[r+1][c] = null;
+                    copiedShelf[r][c+1] = null;
+                    copiedShelf[r+1][c+1] = null;
                     squareCounter++;
-                    squareFlag=true;
-                    c=c+2;
-                    squareColumn=c;
+                    c++;
                 }
-                if (squareCounter==2){
+                if (squareCounter>=2){
                     return true;
                 }
             }
