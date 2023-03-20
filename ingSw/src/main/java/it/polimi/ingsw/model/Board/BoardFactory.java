c43d89223213c2eb63db6fb153b2e9adc86b7665
DenSandu <denisgabriel.sanduleanu@mail.polimi.it>
2023-03-20 16:31:23 +0100

    risolto file pom.xml

--- ingSw/src/main/java/it/polimi/ingsw/model/Board/BoardFactory.java
+++ ingSw/src/main/java/it/polimi/ingsw/model/Board/BoardFactory.java
@@ -3,16 +3,12 @@ package it.polimi.ingsw.model.Board;
 public class BoardFactory {
 
     public Board createBoard(int players){
-        switch (players){
-            case 2:
-                return new TwoBoard(2);
-            case 3:
-                return new ThreeBoard(3);
-            case 4:
-                return new FourBoard(4);
-            default:
-                throw new IllegalArgumentException("Invalid number of boards: " + players);
-        }
+        return switch (players) {
+            case 2 -> new TwoBoard(2);
+            case 3 -> new ThreeBoard(3);
+            case 4 -> new FourBoard(4);
+            default -> throw new IllegalArgumentException("Invalid number of boards: " + players);
+        };
 
     }
 }
