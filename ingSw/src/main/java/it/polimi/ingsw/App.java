f73f2744676794029c49b946e6bd733834d509cf
rivitti01 <francesco.rivitti@mail.polimi.it>
2023-03-21 13:54:08 +0100

    +

--- ingSw/src/main/java/it/polimi/ingsw/App.java
+++ ingSw/src/main/java/it/polimi/ingsw/App.java
@@ -3,11 +3,13 @@ package it.polimi.ingsw;
 import it.polimi.ingsw.model.Bag;
 import it.polimi.ingsw.model.Board.Board;
 import it.polimi.ingsw.model.Board.BoardFactory;
+import it.polimi.ingsw.model.Color;
 import it.polimi.ingsw.model.Tile;
 
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * Hello world!
@@ -23,10 +25,20 @@ public class App
         myBoard.fillBoard(oneBag);
         List<Tile> tiles = new ArrayList<>();
         int i = 0;
-        while (i != 10){
+        while (i != 3){
             tiles.add(oneBag.getTile());
             i++;
         }
+        ListIterator<Tile> tmp = tiles.listIterator();
+        Color c = tmp.next().getColor();
+        c = tmp.next().getColor();
+        c = tmp.next().getColor();
+        c = tmp.next().getColor();// non va oltrw
+        c = tmp.next().getColor();
+        c = tmp.next().getColor();
+        c = tmp.next().getColor();
+        c = tmp.next().getColor();
+
 
 
 
