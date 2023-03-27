2c7c081f33bf03989df240e2a0682f8b0a3de431
rivitti01 <francesco.rivitti@mail.polimi.it>
2023-03-27 15:19:05 +0200

    CommonGoalCard is modular, we can change PlayersPoints.json for change points

--- src/main/java/it/polimi/ingsw/model/CommonGoalCard.java
+++ src/main/java/it/polimi/ingsw/model/CommonGoalCard.java
@@ -1,8 +1,18 @@
 package it.polimi.ingsw.model;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import it.polimi.ingsw.model.Algorythms.CardStrategy;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Stack;
+
+import static it.polimi.ingsw.Costants.SHELF_COLUMN;
+import static it.polimi.ingsw.Costants.SHELF_ROWS;
 
 public class CommonGoalCard {
     private CardStrategy cardStrategy;
@@ -23,24 +33,25 @@ public class CommonGoalCard {
         return scores.pop();
     }
 
-    public void fillStack(int numberPartecipants){ //pro-tip: creare uno strategy pattern/factory method anche per scores
+    public void fillStack(int numberParticipants){
         scores = new Stack<>();
-        switch (numberPartecipants) {
-            case 4 -> {
-                scores.push(2);
-                scores.push(4);
-                scores.push(6);
-                scores.push(8);
+        String filePath = "src/main/resources/PlayersPoints.json";
+        File input = new File(filePath);
+        try {
+            JsonElement fileElement = JsonParser.parseReader(new FileReader(input));
+            JsonObject fileObject = fileElement.getAsJsonObject();
+            JsonArray jsonArrayOfPoints = fileObject.get(String.valueOf(numberParticipants)).getAsJsonArray();
+            for (JsonElement personalElement : jsonArrayOfPoints){
+                JsonObject personalObject = personalElement.getAsJsonObject();
+                int point = personalObject.get("point").getAsInt();
+                scores.push(point);
             }
-            case 3 -> {
-                scores.push(4);
-                scores.push(6);
-                scores.push(8);
-            }
-            case 2 -> {
-                scores.push(4);
-                scores.push(8);
-            }
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found!");
+            e.printStackTrace();
+        } catch (Exception e){
+            System.err.println("File read error!");
+            e.printStackTrace();
         }
     }
 
