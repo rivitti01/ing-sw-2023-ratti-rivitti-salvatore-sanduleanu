d164e68dc79fd478edfc3059cf6f94d9af4149e3
SAAL01 <alex.salv@outlook.it>
2023-04-02 00:30:10 +0200

    CGC 8 adattato per shelf variabile

--- src/main/java/it/polimi/ingsw/model/Algorythms/CommonGoalCard7.java
+++ src/main/java/it/polimi/ingsw/model/Algorythms/CommonGoalCard7.java
@@ -2,21 +2,27 @@ package it.polimi.ingsw.model.Algorythms;
 
 import it.polimi.ingsw.model.Shelf;
 
+import static it.polimi.ingsw.Costants.SHELF_COLUMN;
+import static it.polimi.ingsw.Costants.SHELF_ROWS;
+
 public class CommonGoalCard7 implements CardStrategy {
 
     public boolean algorythm(Shelf myShelf) {
-        //le diagonali possibili sono 4, le controllo una per volta
-        for(int i=0; i<2; i++) {
-            for(int j=0; myShelf.getTile(i+j,i+j).getColor() == myShelf.getTile(i+j+1,i+j+1).getColor(); j++)
-                if(j==3) return true;
-        }
+        for (int r=0; r<SHELF_ROWS; r++)
+            for(int c=0; c<SHELF_COLUMN; c++){   //scorro la shelf
 
-        for(int i=4; i<6; i++) {
-            for(int j=0; myShelf.getTile(i+j,i+j).getColor() == myShelf.getTile(i+j-1,i+j+1).getColor(); j++)
-                if(j==3) return true;
-        }
+                if( (c <= SHELF_COLUMN-5) && (r <= SHELF_ROWS-5) ){  //se posso checkare per una diagonale principale
+                    for(int i=0; myShelf.getTile(i+r,i+c).getColor() == myShelf.getTile(i+r+1,i+c+1).getColor(); i++)
+                        if(i==3) return true;
+                }
 
-        return false;
+                if( (c <= SHELF_COLUMN-5) && (r >= 5) )   {    //se posso checkare per una diagonale secondaria
+                    for(int i=0; myShelf.getTile(i+r,i+c).getColor() == myShelf.getTile(i+r-1,i+c+1).getColor(); i++)
+                        if(i==3) return true;
+                }
+
+            }
+    return false;
     }
 
 
