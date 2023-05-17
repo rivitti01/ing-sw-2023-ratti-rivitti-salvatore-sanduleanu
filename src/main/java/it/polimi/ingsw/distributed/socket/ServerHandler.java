41ee02b731da10212d1bdd4e3dc664cf909d866d
rivitti01 <francesco.rivitti@mail.polimi.it>
2023-05-17 11:34:33 +0200

    inizio socket

--- src/main/java/it/polimi/ingsw/distributed/socket/ServerHandler.java
+++ src/main/java/it/polimi/ingsw/distributed/socket/ServerHandler.java
@@ -1,4 +1,83 @@
 package it.polimi.ingsw.distributed.socket;
 
-public class ServerHandler {
+import it.polimi.ingsw.distributed.Client;
+import it.polimi.ingsw.distributed.Server;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.util.ModelListener;
+import it.polimi.ingsw.util.Warnings;
+
+import java.rmi.RemoteException;
+
+public class ServerHandler implements Server,Runnable, ModelListener {
+    private String nickname;
+
+    @Override
+    public void clientConnection(Client c, String nickName) throws RemoteException {
+
+    }
+
+    @Override
+    public void tileToDrop(int tilePosition) throws RemoteException {
+
+    }
+
+    @Override
+    public void checkingCoordinates(int[] coordinates) throws RemoteException {
+
+    }
+
+    @Override
+    public void columnSetting(int i) throws RemoteException {
+
+    }
+
+    @Override
+    public void endsSelection() throws RemoteException {
+
+    }
+
+    @Override
+    public void numberOfParticipantsSetting(int n) throws RemoteException {
+
+    }
+
+    @Override
+    public void printGame() {
+
+    }
+
+    @Override
+    public void error(Warnings e, Player currentPlayer) {
+
+    }
+
+    @Override
+    public void newTurn(Player currentPlayer) {
+
+    }
+
+    @Override
+    public void askOrder() {
+
+    }
+
+    @Override
+    public void isLastTurn() {
+
+    }
+
+    @Override
+    public void askColumn() {
+
+    }
+
+    @Override
+    public void askAction() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
