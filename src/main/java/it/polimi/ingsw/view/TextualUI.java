80f646ed8e8328ab95a3605a9ae77ed20180a74b
Leonardo Ratti <leonardo.ratti@mail.polimi.it>
2023-06-26 22:15:00 +0200

    gui half fixed

--- src/main/java/it/polimi/ingsw/view/TextualUI.java
+++ src/main/java/it/polimi/ingsw/view/TextualUI.java
@@ -422,7 +422,7 @@ public class TextualUI implements UI {
                 if (this.currentState!=CurrentState.WAITING_FOR_CLIENTS && input.equals("chat")) {
                     oldState = this.currentState;
                     this.currentState = CurrentState.CHATTING;
-                    System.out.println("Scrivi qualcosa a qualcuno");
+                    System.out.println("scrivi qualcosa a qualcuno! (Inizia con @nickname per mandarlo in privato)");
                 } else {
                     try {
                         switch (this.currentState) {
